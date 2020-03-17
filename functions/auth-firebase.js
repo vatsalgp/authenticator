ee52cfa02675dbf492909740556617a621aeca8d
vatsalgp <vatsalgupta280@gmail.com>
2020-03-17 21:35:19 +0530

    improved bcrypt

--- functions/auth-firebase.js
+++ functions/auth-firebase.js
@@ -1,10 +1,10 @@
-const bcrypt = require("bcrypt-nodejs");
+const bcrypt = require("bcrypt");
 const firebase = require("./firebase");
 
 exports.readCred = async email => {
     const user = await firebase.getByEmail(email);
     //User exists
-    if (user && user.email == email)
+    if (user && user.email === email)
         return user.password;
     //User doesn't exist
     else
@@ -12,20 +12,7 @@ exports.readCred = async email => {
 };
 
 exports.writeCred = async ({ email, password }, callback) => {
-    //Generate Salt
-    bcrypt.genSalt(10, async (err, salt) => {
-        if (err) return err;
-
-        //Hash password using salt, then run function
-        bcrypt.hash(password, salt, null, async (err, hash) => {
-            if (err) return err;
-
-            //Overwrite password with hash
-            password = hash;
-
-            //Save the model
-            const id = await firebase.write({ email, password });
-            callback(id);
-        });
-    });
+    const hash = await bcrypt.hash(password, 10);
+    const id = await firebase.write({ email, password: hash });
+    callback(id);
 }
